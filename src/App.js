fa9d2ffa20a0604e572e6302fbaef770e8c989d8
Mohit Kishore <gkmohit@gmail.com>
2018-10-18 15:16:05 -0400

    Added react carousel. BUt thinking if its better to use bootsrap and create your own componenets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Carousel>
+          <div>
+              <img src="https://via.placeholder.com/3500x1500" />
+              <p className="legend">Legend 1</p>
+          </div>
+          <div>
+              <img src="https://via.placeholder.com/3500x1500" />
+              <p className="legend">Legend 2</p>
+          </div>
+          <div>
+              <img src="https://via.placeholder.com/3500x1500" />
+              <p className="legend">Legend 3</p>
+          </div>
+        </Carousel>
       </div>
     );
   }
