cf54a5bcf854e11174510b54c1cfd3bf39a72e46
Mohit Kishore <gkmohit@gmail.com>
2018-10-18 15:59:42 -0400

    added css and images from template

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Carousel>
-          <div>
-              <img src="https://via.placeholder.com/3500x1500" />
-              <p className="legend">Legend 1</p>
-          </div>
-          <div>
-              <img src="https://via.placeholder.com/3500x1500" />
-              <p className="legend">Legend 2</p>
-          </div>
-          <div>
-              <img src="https://via.placeholder.com/3500x1500" />
-              <p className="legend">Legend 3</p>
-          </div>
-        </Carousel>
+        
       </div>
     );
   }
